22ad98f70249b4256a65161f11beaa372c568262
Kyle Rector <krtools@users.noreply.github.com>
2018-11-28 11:00:32 -0500

    Check react element ref before using (can be called after unmount)

--- src/Container.tsx
+++ src/Container.tsx
@@ -16,7 +16,7 @@ export interface ContainerProps {
  * The main container and entry point.
  */
 export default class Container extends React.Component<ContainerProps> {
-  private el: HTMLDivElement;
+  private el?: HTMLDivElement;
 
   private _setRef = (el: HTMLDivElement) => {
     this.el = el;
@@ -25,7 +25,7 @@ export default class Container extends React.Component<ContainerProps> {
   onBlur = () => {
     // blur happens before focus, but is also annoying to manage w/ IE & react 15
     setTimeout(() => {
-      if (!this.el.contains(document.activeElement)) {
+      if (this.el && !this.el.contains(document.activeElement)) {
         this.props.store.blur();
       }
     });
